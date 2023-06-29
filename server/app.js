70a67ecddbcb610c5a9c9c4c6db243bcc770882c
Mai-Elhajeen <maielhajeen2001@gmail.com>
2023-06-30 01:48:19 +0300

    running the server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
 // Add code here 
+const express = require('express');
+const { join } = require('path');
+
+const app = express();
+
+app.use(express.static(join(__dirname, '..', 'public')));
+
+app.set('port', process.env.PORT || 3000);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,6 @@
 // Add code here 
+const app = require("./app");
+
+app.listen(app.get('port'), ()=>{
+    console.log(`App is live on http://localhost:${app.get('port')}`);
+})
